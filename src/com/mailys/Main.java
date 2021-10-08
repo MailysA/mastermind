5483df62ca5f3f665c41131ab0e06ab4bd7e7dac
Mailys A <sandboxmailys@gmail.com>
2021-10-08 23:28:00 +0200

    refaccto

--- src/com/mailys/Main.java
+++ src/com/mailys/Main.java
@@ -26,55 +26,38 @@ public class Main {
         if(playerChoiceReader == null) {
             System.out.println("Il faut renseigner une suite de 4 chiffres.");
         }else {
-            String currentResult = "";
-            while(currentResult != secret.toString()) {
+            StringBuilder currentResultBuilder = new StringBuilder();
+            while(currentResultBuilder.toString() != secret.toString()) {
                 /* Choix du joueur */
                 String playerChoice = playerChoiceReader.readLine().trim();
                 System.out.println("Choix du joueur: " + playerChoice);
 
                 /* controle du choix du joueur */
                 while (playerChoice.length() != 4) {
-                    if (playerChoice.length() < 4 || playerChoice.length() > 4) {
-                        System.out.println("Il faut rentrer 4 nombres, réessayez:");
-                        playerChoiceReader = new BufferedReader(new InputStreamReader(System.in));
-                        playerChoice = playerChoiceReader.readLine().trim();
-                    }
+                    System.out.println("Il faut rentrer 4 nombres, réessayez:");
+                    playerChoiceReader = new BufferedReader(new InputStreamReader(System.in));
+                    playerChoice = playerChoiceReader.readLine().trim();
                 }
 
                 char[] playerChoiceArray = playerChoice.toCharArray();
-                for (int i = 0; i <= playerChoiceArray.length; i++) {
-                    System.out.println("choix joueur: " + playerChoiceArray[i]);
-                    for (int j = 0; j <= secret.length; j++) {
-                        if (playerChoiceArray[i] == secret[j]) {
-                            currentResult += "+";
-                        } else if (Arrays.asList(secret).contains(playerChoiceArray[i])) {
-                            currentResult += "-";
+                for (int i = 0; i < playerChoiceArray.length; i++) {
+                    for (int j = 0; j < secret.length; j++) {
+                    System.out.println("choix joueur: " + Character.getNumericValue(playerChoiceArray[i]));
+                    int currentDigit = Character.getNumericValue(playerChoiceArray[i]);
+                    System.out.println("secret à la position: " + secret[j]);
+                        if (currentDigit == secret[j]) {
+                            currentResultBuilder.append("+");
+                        } else if (Arrays.asList(secret).contains(currentDigit)) {
+                            currentResultBuilder.append("-");
                         } else {
-                            currentResult += "";
+                            currentResultBuilder.append(" ");
                         }
                     }
                 }
-                System.out.println("résultat: " + currentResult);
+                System.out.println("résultat: " + currentResultBuilder);
             }
-            System.out.println("Bien joué! vous avez trouvé la combinaison secrète" + currentResult);
+            System.out.println("Bien joué! vous avez trouvé la combinaison secrète" + currentResultBuilder);
 
         }
-	/*
-	 Pour chaque partie faire
-	    random secret 4 caracteres à chaque partie
-        proposition joueur en input
-        comparaison du secret et de l'input (tableau indexé)
-        tant que secret match pas avec input du joueur alors
-        Pour chaque matching d'index
-            si le nombre est le meme alors
-                log "+"
-            sinon si le nombre est contenu dans le tableau alors
-            log "-"
-            sinon
-            log "vide"
-            fin si
-        fin pour
-    fin pour
-	 */
     }
 }
