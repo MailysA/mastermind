b0a7f96de4335398049e99c149eac79ac831998a
Mailys A <sandboxmailys@gmail.com>
2021-10-15 20:46:35 +0200

    fix: final result

--- src/com/mailys/Main.java
+++ src/com/mailys/Main.java
@@ -3,13 +3,61 @@ package com.mailys;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /* initialise du secret */
+        int[] secret = getSecret();
+        ArrayDeque result;
+        StringBuilder showResult = null;
+        boolean endGame = false;
+        while(!endGame) {
+            showResult = new StringBuilder();
+            System.out.println("Veuillez entrer 4 chiffres:");
+            BufferedReader playerChoiceReader = new BufferedReader(new InputStreamReader(System.in));
+            if(playerChoiceReader == null) {
+                System.out.println("Il faut renseigner une suite de 4 chiffres.");
+            } else {
+                result = new ArrayDeque<>();
+                /* Choix du joueur */
+                String playerChoice = playerChoiceReader.readLine().trim();
+                System.out.println("Choix du joueur: " + playerChoice);
+
+                /* controle du choix du joueur */
+                while (playerChoice.length() != 4) {
+                    System.out.println("Il faut rentrer 4 chiffres, réessayez:");
+                    playerChoiceReader = new BufferedReader(new InputStreamReader(System.in));
+                    playerChoice = playerChoiceReader.readLine().trim();
+                }
+
+                char[] playerChoiceArray = playerChoice.toCharArray();
+                for(int i = 0; i < 4; i++) {
+                    int currentDigitPlayer = Character.getNumericValue(playerChoiceArray[i]);
+                        if (currentDigitPlayer == secret[i]) {
+                            result.addFirst("+");
+                        } else if (IntStream.of(secret).anyMatch(x -> x == currentDigitPlayer)) {
+                            result.addLast("-");
+                        } else {
+                            result.push("");
+                        }
+                    }
+                StringBuilder finalShowResult = showResult;
+                result.stream().forEach(x -> finalShowResult.append(x));
+                System.out.println("résultat: " + showResult);
+                if(result.stream().allMatch( x -> x == "+")){
+                    endGame = true;
+                }
+             }
+        }
+        System.out.println("Bien joué! vous avez trouvé la combinaison secrète " +  showResult);
+        System.out.println("Merci d'avoir joué au Mastermind !");
+    }
+
+    private static int[] getSecret() {
+        /* initialise secret */
         int Min = 0;
         int Max = 9;
         int number1secret =  Min + (int)(Math.random() * ((Max - Min) + 1));
@@ -17,47 +65,9 @@ public class Main {
         int number3secret = Min + (int)(Math.random() * ((Max - Min) + 1));
         int number4secret =  Min + (int)(Math.random() * ((Max - Min) + 1));
 
-        /* generation du secret */
+        /* generate secret */
         int[] secret = new int[]{number1secret,number2secret,number3secret,number4secret};
         System.out.println("Generation du secret: " + number1secret + " " + number2secret + " " + number3secret + " " + number4secret);
-
-        System.out.println("Veuillez entrer 4 chiffres entiers:");
-        BufferedReader playerChoiceReader = new BufferedReader(new InputStreamReader(System.in));
-        if(playerChoiceReader == null) {
-            System.out.println("Il faut renseigner une suite de 4 chiffres.");
-        }else {
-            StringBuilder currentResultBuilder = new StringBuilder();
-            while(currentResultBuilder.toString() != secret.toString()) {
-                /* Choix du joueur */
-                String playerChoice = playerChoiceReader.readLine().trim();
-                System.out.println("Choix du joueur: " + playerChoice);
-
-                /* controle du choix du joueur */
-                while (playerChoice.length() != 4) {
-                    System.out.println("Il faut rentrer 4 nombres, réessayez:");
-                    playerChoiceReader = new BufferedReader(new InputStreamReader(System.in));
-                    playerChoice = playerChoiceReader.readLine().trim();
-                }
-
-                char[] playerChoiceArray = playerChoice.toCharArray();
-                for (int i = 0; i < playerChoiceArray.length; i++) {
-                    for (int j = 0; j < secret.length; j++) {
-                    System.out.println("choix joueur: " + Character.getNumericValue(playerChoiceArray[i]));
-                    int currentDigit = Character.getNumericValue(playerChoiceArray[i]);
-                    System.out.println("secret à la position: " + secret[j]);
-                        if (currentDigit == secret[j]) {
-                            currentResultBuilder.append("+");
-                        } else if (Arrays.asList(secret).contains(currentDigit)) {
-                            currentResultBuilder.append("-");
-                        } else {
-                            currentResultBuilder.append(" ");
-                        }
-                    }
-                }
-                System.out.println("résultat: " + currentResultBuilder);
-            }
-            System.out.println("Bien joué! vous avez trouvé la combinaison secrète" + currentResultBuilder);
-
-        }
+        return secret;
     }
 }
